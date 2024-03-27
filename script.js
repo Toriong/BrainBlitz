6cbe653531458f02bc3360deb8a74af3aa4a89b6
Toriong <gtorion97@gmail.com>
2024-03-27 12:22:53 -0500

    creating the game logic, instanting the cells

---
+++ script.js
@@ -0,0 +1,7 @@
+// load all of your js here
+
+//GOAL: display the dots onto the ui
+// -the dots are displayed onto the ui
+// -insert  the dots for each cell
+// -a table has been created, each cell is a square
+// -create a table for the dots
