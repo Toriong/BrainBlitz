c128618993da4216d4ba1b886ba1522dd49aae5a
Toriong <gtorion97@gmail.com>
2024-03-31 18:33:35 -0500

    making main menu page responsive

--- game/static/js/mainMenu.js
+++ game/static/js/mainMenu.js
@@ -1,5 +1,31 @@
+import { Button } from "./dom/classes.js";
+import { addCssClasses } from "./dom/fns.js";
 
 
 (() => {
+    function handleOnPgDisplay() {
+        const dots = Array.from($('#dot-1, #dot-2, #dot-3'))
+        const loadingTxtDiv = $('#loading-main-menu-pg')
+        const btnSec = $('.btn-sec-main-menu-pg')
+        // const userBtn = new Button()
 
+        setTimeout(() => {
+            addCssClasses(dots[0], 'jump-element')
+
+            setTimeout(() => {
+                addCssClasses(dots[1], 'jump-element')
+
+                setTimeout(() => {
+                    addCssClasses(dots[2], 'jump-element')
+                }, 300)
+            }, 300);
+        })
+
+        setTimeout(() => {
+            addCssClasses(loadingTxtDiv, 'fade-element-out')
+
+        }, 400)
+    }
+
+    handleOnPgDisplay()
 })();
